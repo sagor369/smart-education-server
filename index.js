26c4abb2ea531a74406d459c4107eae18c61bc77
Sagor <sagor.heaking@gmail.com>
2023-06-08 23:11:38 +0600

    mongodb server site added

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.MD_User}:${process.env.MD_Pass}@cluster0.nsogw9w.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const populerClass = client.db("smartDb").databaseName.collection(populer);
+
+
+    await client.connect();
+
+    app,get('populer', async(req, les)=?{
+ 
+    })
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir); 
+
+
+
+
+
 
 app.get('/', (req, res) =>{
     res.send('assignment 12 projects running')
